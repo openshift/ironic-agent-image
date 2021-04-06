9d9b2674d867ef4deddcd524671521ee4980a529
Dmitry Tantsur <dtantsur@protonmail.com>
2021-04-06 15:47:49 +0200

    Reduce the dependencies

--- prepare-image.sh
+++ prepare-image.sh
@@ -2,13 +2,17 @@
 
 set -euxo pipefail
 
+echo "install_weak_deps=False" >> /etc/dnf/dnf.conf
+# Tell RPM to skip installing documentation
+echo "tsflags=nodocs" >> /etc/dnf/dnf.conf
+
 dnf install -y python3 python3-requests
 curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
-grep -vE '^(#|$)' /tmp/${PKGS_LIST} | xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y
+grep -vE '^(#|$)' /tmp/${PKGS_LIST} | xargs -rtd'\n' dnf install -y
 if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
     if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
-        grep -vE '^(#|$)' /tmp/${EXTRA_PKGS_LIST} | xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y
+        grep -vE '^(#|$)' /tmp/${EXTRA_PKGS_LIST} | xargs -rtd'\n' dnf install -y
     fi
 fi
 
