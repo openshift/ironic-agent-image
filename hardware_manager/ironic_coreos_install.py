8b6cee4a0a6af7ade904ebbc44d5d24d062df1fa
Zane Bitter <zbitter@redhat.com>
2022-01-13 11:21:16 -0500

    Revert "Remove the default network config before installation"
    Since we only pass --copy-network when network data is provided, there
    is no need to remove the default_connection.nmstate file (which may have
    been overwritten by the user in Ignition).
    
    This reverts commit c4455ff72da0bdf0c990454f59cf4b2fc11558be.

--- hardware_manager/ironic_coreos_install.py
+++ hardware_manager/ironic_coreos_install.py
@@ -87,12 +87,6 @@ class CoreOSInstallHardwareManager(hardware.HardwareManager):
         copy_network = meta_data.get('coreos_copy_network',
                 os.getenv('IPA_COREOS_COPY_NETWORK', '').lower() == 'true')
         if copy_network:
-            try:
-                os.unlink(os.path.join(ROOT_MOUNT_PATH, 'etc',
-                                       'NetworkManager/system-connections',
-                                       'default_connection.nmconnection'))
-            except FileNotFoundError:
-                pass
             args += ['--copy-network']
 
         command = ['chroot', ROOT_MOUNT_PATH,
